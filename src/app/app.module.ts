b31243a270e4ba961193209ab99316d05bd587ef
ChenYn <chenyn01@yuanfudao.com>
2021-04-25 16:06:09 +0800

    chore: upgrade angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, CommonModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,17 @@
-import type { UserConfig } from "vite";
+import type { UserConfig } from 'vite';
 
 const config: UserConfig = {};
 
-export default config;
+export default {
+  ...config,
+  optimizeDeps: {
+    include: [
+      'zone.js/dist/zone',
+      '@angular/core',
+      '@angular/platform-browser-dynamic',
+      '@angular/platform-browser',
+      '@angular/common',
+    ],
+    exclude: ['rxjs'],
+  },
+};
